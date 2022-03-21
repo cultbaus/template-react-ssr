0af29f8dd361b7adb02a9684cd6322f1b976697e
cultbaus <cultbaus@gmail.com>
2022-03-20 19:49:10 -0700

    feat(router): add pages + routes

---
+++ src/pages/About.tsx
@@ -0,0 +1,5 @@
+export const About: React.FC<{}> = () => {
+  return <h1>About page</h1>;
+};
+
+export default About;
---
+++ src/pages/Extra.tsx
@@ -0,0 +1,5 @@
+export const Extra: React.FC<{}> = () => {
+  return <h1>Extra page</h1>;
+};
+
+export default Extra;
